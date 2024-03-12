5ff2fbbefa311be1e0fb7eeb8e9e7fe811998282
Davey-H-7 <138510681+Davey-H-7@users.noreply.github.com>
2024-03-12 14:17:04 +0000

    added contact-form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AlbumComponent } from './album/album.component';
 import { AlbumsComponent } from './albums/albums.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AlbumComponent, AlbumsComponent],
+  imports: [RouterOutlet, AlbumComponent, AlbumsComponent, ContactFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
